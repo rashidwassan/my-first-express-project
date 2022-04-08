996b745bc2b2487bf5689eff9f9acefb2af5c054
rashidwassaan <rashidwassaan@gmail.com>
2022-04-08 21:17:09 +0500

    added comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express =  require('express');
 const app = express();
 
 //defining the port where the server will run
@@ -7,5 +7,5 @@ const port = 3000;
 // this is the typical example of using express to serve static files
 app.get('/', (req, res)=> res.send('Hello World!'))
 
-// listening on the port
-app.listen(port, ()=> console.log('Example app listening on port' + port))
+// listening on the port, it is kind of defining on which place we can see the response.
+app.listen(port, ()=> console.log('Example app listening on port ' + port))
